59520a4c4b172b8e254136403ba9aa083e6c7e1f
lwilhem <wilhem.lecanu@edu.devinci.fr>
2024-02-05 00:04:56 +0100

    feat: single page

--- packages/app/src/components/RecipeListElement.vue
+++ packages/app/src/components/RecipeListElement.vue
@@ -17,7 +17,7 @@ onMounted(() => {
     <div class="bg-zinc-950 text-slate-50 rounded-md p-4 w-80 mx-2">
       <h2 class="font-bold uppercase text-lg font-mono">{{ recipe.recipe_name }}</h2>
       <p class="text-sm">{{ recipe.recipes_description }}</p>
-      <NuxtLink :to="`/recipes/${recipe.slug}`" class="text-sm no-underline bg-green-500 text-zinc-950 px-4 rounded-sm">
+      <NuxtLink :to="`/recipes/${recipe.id}`" class="text-sm no-underline bg-green-500 text-zinc-950 px-4 rounded-sm">
         voir plus
       </NuxtLink>
     </div>
--- packages/app/src/pages/recipes/[id].vue
+++ packages/app/src/pages/recipes/[id].vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import type { IStrapiRecipe } from '~/models/search.model';
+
+const route = useRoute()
+const { findOne } = useStrapi()
+const recipe = ref()
 
 onMounted(() => {
-  console.log('mounted')
+  findOne<IStrapiRecipe>('Recipes', route.params.id as string).then(({data}) => {
+    recipe.value = data
+  })
 })
 </script>
 
 <template>
-  <span>page</span>
+  <div v-if="recipe">
+    <h1>{{ recipe.recipe_name }}</h1>
+    <p>{{ recipe.recipes_description }}</p>
+  </div>
 </template>
